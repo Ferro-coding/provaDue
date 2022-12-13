26f9e761a0ac7f15012ec27f21b42dd3627486d0
Ferro-coding <95420374+Ferro-coding@users.noreply.github.com>
2022-12-13 17:40:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const http = require('http');
-
+//const http = require('http');
+const express = require('express')
+//const {readFileSync, writeFileSync} = require('fs')
+const app = express()
+/*
 const hostname = '0.0.0.0';
 
 const port = 3000;
@@ -9,8 +12,15 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Zeet Node');
 });
-
-
+*/
+/*
 server.listen(port, hostname, () => { 
     console.log('Server running at http://${hostname}:${port}/');
 });
+*/
+
+app.get('/',function (req, res) {
+    res.sendFile('login.html', {root: __dirname })
+})
+
+//app.listen(3000);
