8a8110419bddd56bec49bca425091d874c534588
Ferro-coding <attilioferrise@gmail.com>
2022-12-14 17:58:13 +0100

    aggiunta

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.sendFile('login.html', {root: __dirname + '/public'}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
